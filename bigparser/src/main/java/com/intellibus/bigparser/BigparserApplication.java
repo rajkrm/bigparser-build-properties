61c607eb55ba3f46133fbebe53493cb89f4317fb
Raj <kiran.munnangi@gmail.com>
2022-12-15 09:18:52 -0500

    initial commit

---
+++ bigparser/src/main/java/com/intellibus/bigparser/BigparserApplication.java
@@ -0,0 +1,13 @@
+package com.intellibus.bigparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigparserApplication.class, args);
+	}
+
+}
---
+++ bigparser/src/test/java/com/intellibus/bigparser/BigparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellibus.bigparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
